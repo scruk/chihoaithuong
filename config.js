83722cafe134c9be78280e5c3c0a758cdc983dfd
phuctho509 <132209931+scruk@users.noreply.github.com>
2023-11-07 09:53:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
-    introTitle: 'Xin chào các SUNer',
+    introTitle: 'Em chào chị Thương',
     introDesc: `Từ lần đầu tiên gặp nhau
-    em/tui đã nghĩ chúng mình là 
+    em đã nghĩ chúng mình là 
     My Đẹt ty ni của nhau rồi`,
     btnIntro: 'kệ mày',
-    title: 'Phải chăng các anh chị và mọi người cũng thấy chúng ta là định mệnh thanh xuân của nhau😙',
+    title: 'Phải chăng chị chúng ta là định mệnh thanh xuân của nhau😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'chắc chắn là như dậy rồi <33',
     btnNo: 'Không, tao không thấy thế :3',
-    question:'Cho em/tui biết lý do mọi người nghĩ như vậy với ạ',
+    question:'Cho em biết lý do chị nghĩ như vậy với ạ',
     btnReply: 'Nè em',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Em/tui biết mà 🥰. Yêu mọi người nhiều nhiều 😘😘',
-    messDesc: 'Như đã nói ở trên thì em/tui nghĩ chúng ta gặp được nhau đã là định mệnh. Tương lai thì không nói trước được nhưng em/tui luôn mong mọi người hãy luôn trao cho nhau những tình cảm, những yêu thương cho nhau như ngày đầu nhé!',
-    btnAccept: 'Nhấn vào đây để nhận quà từ em/tui',
-    messLink: 'https://drive.google.com/drive/folders/1aKTyla_rG11DD0adIaZV4ExinLbC6bJ5?usp=sharing' 
+    mess: 'Em biết mà 🥰. Yêu mọi người nhiều nhiều 😘😘',
+    messDesc: 'Em chào chị Hoài Thương xinh đẹp, là đứa em thẳng thắn của chị đây. Khoảng thời gian 1 năm vừa qua rất đặc biệt với em vì có mọi người trong gia đình SUN và có người chị cả như chị chỉ dẫn tụi em rất tận tình, chị luôn mang đến cho em nguồn năng lượng tích cực, giúp em hoàn thiện bản thân với những lời khuyên sâu sắc, em và mọi người trong team rất biết ơn chị và mong chị sẽ tiếp tục vai trò dẫn dắt tụi em. Em chúc chị ngày càng xinh đẹp, thật nhiều sức khỏe và ngày càng thành công nhé!',
+    btnAccept: 'ấn vào đây nếu chị thấy sến',
+    messLink: 'https://youtu.be/4pTjTLbw82M?feature=shared&t=75' 
 }
